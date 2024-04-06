e93c6c04ea886c0287bd1794badb8a9b4ecea981
hermesmarcano <104464766+hermesmarcano@users.noreply.github.com>
2024-04-07 00:12:49 +0200

    starting the calculadora project, make initial set up, add database and types

---
+++ calculadora-propinas/src/App.tsx
@@ -0,0 +1,18 @@
+import { menuItems } from "./data/db"
+
+function App() {
+
+console.log(menuItems);
+
+
+  return (
+    <>
+    <header className="bg-teal-400 py-5 ">
+    <h1 className="text-4xl text-center font-black">Calculadora</h1>
+    </header>
+      
+    </>
+  )
+}
+
+export default App
