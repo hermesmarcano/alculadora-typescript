72444852ac16e0c8e91fc952c5cabb72444a08a0
hermesmarcano <104464766+hermesmarcano@users.noreply.github.com>
2024-05-05 20:09:06 +0200

    Readh for Deployment (need to add memory storage)

--- calculadora-propinas/src/App.tsx
+++ calculadora-propinas/src/App.tsx
@@ -1,16 +1,48 @@
+import MenuItem from "./components/MenuItem";
+import OrderContent from "./components/OrderContent";
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder";
+
 
 function App() {
 
-console.log(menuItems);
-
+const {order, addItem, removeItem, placeOrder} = useOrder()
 
   return (
     <>
     <header className="bg-teal-400 py-5 ">
     <h1 className="text-4xl text-center font-black">Calculadora</h1>
     </header>
-      
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="mr-10">
+        <h2 className="text-4xl font-black ">Menu</h2>
+         <div className="space-y-3 mt-10">
+        {menuItems.map(item => (
+          <MenuItem
+          key={item.id}
+          item={item}
+          addItem={addItem}/>
+        ))}
+         </div>
+        
+        </div>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+        <h2 className='font-black text-4xl'>Consumo</h2>
+        {order.length > 0 ? (
+          <>
+          <OrderContent
+        order={order}
+        removeItem={removeItem}
+        placeOrder={placeOrder}/>
+          </>
+        ) 
+        : <p className="text-center">La Orden Esta Vacia</p>}
+        
+        </div>
+        
+        
+      </main>
     </>
   )
 }
